ece79b6206b9350f891383a74c19a269cbc7789b
grischat <kosolapg2@gmail.com>
2023-09-06 00:00:22 +0200

    -First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import InputField from './components/InputField/InputField'
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+      <h4>
+      Password Generator
+      </h4>
+      <InputField/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,14 @@
+import '../InputField/InputField.scss'
+
+const InputField = () => {
+    return (
+        <div>
+            <input className="input__container" placeholder='Click Generate ->'>
+                
+            </input>
+            <div></div>
+        </div>
+    )
+}
+
+export default InputField;
